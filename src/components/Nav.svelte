79dc4cba1dd6666d29bfb35204393b17216cf36c
stephenlai <stephenlai2015@gmail.com>
2020-05-05 10:53:55 +0800

    remove about & blog menu in navbar

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -51,7 +51,7 @@
 <nav>
 	<ul>
 		<li><a aria-current='{segment === undefined ? "page" : undefined}' href='.'>home</a></li>
-		<li><a aria-current='{segment === "about" ? "page" : undefined}' href='about'>about</a></li>
-		<li><a aria-current='{segment === "blog" ? "page" : undefined}' href='blog'>blog</a></li>
+		<!-- <li><a aria-current='{segment === "about" ? "page" : undefined}' href='about'>about</a></li>
+		<li><a aria-current='{segment === "blog" ? "page" : undefined}' href='blog'>blog</a></li> -->
 	</ul>
 </nav>
